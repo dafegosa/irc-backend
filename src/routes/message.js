2d5cd80a8cca6a2fc29e0fdc8b15d4bbe5bd10e1
dafegosa <danielgomez.s@hotmail.com>
2021-01-31 23:52:02 -0500

    Create auth.js file and use it in messages routes

--- src/routes/message.js
+++ src/routes/message.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 const msgController = require('../controllers/msg.controller')
+const { auth } = require('../utils/auth')
 
-router.route('/create').post(msgController.create)
-router.route('/get').get(msgController.list)
+router.route('/create').post(auth, msgController.create)
+router.route('/get').get(auth, msgController.list)
 // router.route('/signin').post(msgController.signIn)
 module.exports = router
